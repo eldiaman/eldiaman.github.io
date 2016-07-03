6e9a787f53321f7bbdc9b980fff9ea80e910a7c6
RazZa <el.diamantakos@gmail.com>
2016-07-03 18:29:14 +0200

    Update

--- func.js
+++ func.js
@@ -1,26 +1,27 @@
-function setCookie(cname, cvalue, exdays) {
+	function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + "; " + expires;
-}
+	}
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length,c.length);
-        }
-    }
-    return "";
-}
+	function getCookie(cname) {
+		var name = cname + "=";
+		var ca = document.cookie.split(';');
+		for(var i = 0; i <ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length,c.length);
+			}
+		}
+		return "";
+	}
 
-function setResults(lin, log, kin, nat, vis, mus, inter, intra) {
-	s = lin + " " + log + " " + kin + " " + nat + " " + vis + " " + mus + " " + inter + " " + intra;
-	alert(s);
-}
+	function setResults(lin, log, kin, nat, vis, mus, inter, intra) {
+		s = lin + " " + log + " " + kin + " " + nat + " " + vis + " " + mus + " " + inter + " " + intra;
+		
+		return s;
+	}
